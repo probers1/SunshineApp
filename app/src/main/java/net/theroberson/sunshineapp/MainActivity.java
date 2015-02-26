fe690d3b4780478436fec902e435bcc6ef723129
Paul Roberson <probers1@gmail.com>
2015-02-26 16:07:58 -0600

    Time to leave...saving to work from home.

--- app/src/main/java/net/theroberson/sunshineapp/MainActivity.java
+++ app/src/main/java/net/theroberson/sunshineapp/MainActivity.java
@@ -1,13 +1,10 @@
 package net.theroberson.sunshineapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -40,43 +37,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            // Once the root view for the Fragment has been created, it's time
-            // the ListView with some dummy data.
-
-            // Create some dummy data for the ListVIew.  Here's a sample weekly
-            // represented as "day, weather, high/low"
-
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/40",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Asteroids - 75/65",
-                    "Fri - Heavy Rain - 65/56",
-                    "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51",
-                    "sun - Sunny - 80/68"
-            };
-
-
-            return rootView;
-        }
-    }
 }
